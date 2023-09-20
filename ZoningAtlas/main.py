096a2e453504a59ab2b23868ca5e9dc5b365ecfb
Yoshi Bird <92479166+thebestyoshibird@users.noreply.github.com>
2023-09-20 13:43:52 -0400

    Added new functions
    Created and differentiated functions to load .csv data into pd.DataFrame and .geoJSON data into pd.GeoDataFrame, then concatenate loaded data into single object.

--- ZoningAtlas/main.py
+++ ZoningAtlas/main.py
@@ -1,11 +1,39 @@
+import os
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import geopandas as gpd
 
-def load_jxtn(filename):
+def consolidate_districts(folder, gis_or_zoning): # gis_or_zoning is a string variable with value of either 'gis' or 'zoning'
+    counter = 0
+    base_dir = os.path.join(os.getcwd(), folder)
+    for gjson in os.listdir(base_dir):
+        thisfile = gjson
+        f = os.path.join(base_dir, thisfile)
+        with open(f, 'r') as filename:
+            if gis_or_zoning == 'gis':
+                this_jxtn = load_gis(f)
+            else:
+                if gis_or_zoning == 'zoning':
+                    this_jxtn = load_zoning(filename)
+                else:
+                    print('Need to specify type of dataframe')
+                    return(-1)
+            if counter == 0:
+                all_VT_zoning = this_jxtn
+                counter = 1
+            else:
+                all_VT_zoning = pd.concat([this_jxtn, all_VT_zoning])
+    return all_VT_zoning
+
+def load_gis(filename):
     gdf = gpd.read_file(filename)
     return gdf
+
+def load_zoning(filename): # can be used to load .csv of either jurisdiction or district data
+    df = pd.read_csv(filename)
+    return df
 
 def get_areas(gdf):
     gdf_copy = gdf.copy()
@@ -22,20 +50,28 @@ def by_right(housingtype, gdf):
     by_right = pd.DataFrame(gdf.loc[gdf[housingtype] == "Allowed/Conditional"], columns = gdf.columns)
     return by_right
 
-# Load jurisdiction
-bolton = load_jxtn('Bolton.geojson')
+# Load data for all jurisdictions in directory into a single GeoDataFrame
+all_VT_data = consolidate_districts('data', 'gis')
+
+''' Jurisdiction characteristics '''
+
+# Total number of jurisdictions analyzed
+print('Total number of jurisdictions: ', len(all_VT_data['Jurisdiction'].unique()))
+
+# Number and percentage of jurisdictions with zoning
+print('Number of jurisdictions with zoning: ', all_VT_data['Zoning'].values_counts()['Yes'])
 
 # Calculate areas of all districts in jurisdiction
-bolton_areas = get_areas(bolton)
-
-# Calculate total area of jurisdiction by summing areas of base districts only,
-# then converting square meters to square miles
-
-totalarea = get_total_area(bolton_areas)
-print('The total area in square miles of Bolton is: ' + str(totalarea))
-
-# Calculate area of districts where single-family housing is allowable by right
-singlefamily = np.sum(by_right('1-Family Treatment', bolton_areas)['area'])
-print('The area of districts where single-family housing is allowable by right is: ', str(singlefamily))
-
-print('Single-family by right represents ', str(round(100*singlefamily/totalarea, 2)), '% of the area in Bolton.')
+# bolton_areas = get_areas(bolton)
+#
+# # Calculate total area of jurisdiction by summing areas of base districts only,
+# # then converting square meters to square miles
+#
+# totalarea = get_total_area(bolton_areas)
+# print('The total area in square miles of Bolton is: ' + str(totalarea))
+#
+# # Calculate area of districts where single-family housing is allowable by right
+# singlefamily = np.sum(by_right('1-Family Treatment', bolton_areas)['area'])
+# print('The area of districts where single-family housing is allowable by right is: ', str(singlefamily))
+#
+# print('Single-family by right represents ', str(round(100*singlefamily/totalarea, 2)), '% of the area in Bolton.')
