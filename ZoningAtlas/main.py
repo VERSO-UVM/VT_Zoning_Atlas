716e9808861280a101a52f8ddb24bb4c8d4453ae
Yoshi Bird <92479166+thebestyoshibird@users.noreply.github.com>
2023-09-21 13:16:27 -0400

    Continued buildout
    Added more jurisdiction files to assist with testing; added more functions for cross-state comparative analytics

--- ZoningAtlas/main.py
+++ ZoningAtlas/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import geopandas as gpd
 
-def consolidate_districts(folder, gis_or_zoning): # gis_or_zoning is a string variable with value of either 'gis' or 'zoning'
+def consolidate_data(folder, gis_or_zoning): # gis_or_zoning is a string variable with value of either 'gis' or 'zoning'
     counter = 0
     base_dir = os.path.join(os.getcwd(), folder)
     for gjson in os.listdir(base_dir):
@@ -50,16 +50,49 @@ def by_right(housingtype, gdf):
     by_right = pd.DataFrame(gdf.loc[gdf[housingtype] == "Allowed/Conditional"], columns = gdf.columns)
     return by_right
 
-# Load data for all jurisdictions in directory into a single GeoDataFrame
-all_VT_data = consolidate_districts('data', 'gis')
+# Load data for all districts in directory into a single GeoDataFrame
+# all_district_geodata = consolidate_data('districts', 'gis')
+# all_district_zoning_data = consolidate_data('districts', 'zoning')
+
+# Load data for all jurisdictions in directory into a single DataFrame
+all_jxtn_data = consolidate_data('jxtns', 'zoning')
 
 ''' Jurisdiction characteristics '''
 
 # Total number of jurisdictions analyzed
-print('Total number of jurisdictions: ', len(all_VT_data['Jurisdiction'].unique()))
+print('Total number of jurisdictions: ', len(all_jxtn_data))
+
+# Side-by-side barplot of jurisdiction types both with and without zoning
+govt_types = all_jxtn_data.groupby(['Type of Government', 'Does It Have Zoning?']).size().reset_index(name='count')
+xlabels = []
+heights = []
+for index, row in govt_types.iterrows():
+    if govt_types.loc[index]['Does It Have Zoning?'] == 'Yes':
+        zoninglabel = ' With Zoning'
+    elif govt_types.loc[index]['Does It Have Zoning?'] == 'No':
+        zoninglabel = ' Without Zoning'
+    xlabels.append(govt_types.loc[index]['Type of Government'] + zoninglabel)
+    heights.append(govt_types.loc[index]['count'])
+
+yticks = np.arange(min(heights)-1, max(heights)+1, max(heights)-min(heights))
+plt.bar(xlabels, heights)
+plt.xticks(rotation = 45)
+plt.yticks(yticks)
+plt.title('Types of Jurisdictional Government in Dataset')
+plt.tight_layout()
+plt.show()
 
 # Number and percentage of jurisdictions with zoning
-print('Number of jurisdictions with zoning: ', all_VT_data['Zoning'].values_counts()['Yes'])
+print('Number of jurisdictions with zoning: ', all_jxtn_data['Does It Have Zoning?'].value_counts()['Yes'])
+print('Percentage of jurisdictions with zoning: ', all_jxtn_data['Does It Have Zoning?'].value_counts()['Yes']/len(all_jxtn_data))
+
+# Total # pages of zoning text analyzed
+print(all_jxtn_data['# of Pages in the Zoning Code'])
+# all_jxtn_data['# of Pages in the Zoning Code'].astype(int, copy = False)
+print('Total number of pages of zoning text analyzed: ', np.sum(all_jxtn_data['# of Pages in the Zoning Code']))
+
+# Average # pages in each zoning text
+print('Average number of pages per zoning text: ', np.average(all_jxtn_data['# of Pages in the Zoning Code']))
 
 # Calculate areas of all districts in jurisdiction
 # bolton_areas = get_areas(bolton)
