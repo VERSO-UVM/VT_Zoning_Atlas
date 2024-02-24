72997d53371e45e733d1aebfa84ff0e30ccf8945
Tucker Schulz <104844788+tuckers15@users.noreply.github.com>
2024-02-24 15:22:57 -0500

    setting up workflow as functions

--- ZoningAtlas/ArcPy/script.py
+++ ZoningAtlas/ArcPy/script.py
@@ -1,45 +1,49 @@
 #importing arcpy tools
-import arcpy
+import arcpy #this is just to avoide annoying warnings while editing in ide
 
-
-# before further work: https://learn.arcgis.com/en/projects/automate-a-geoprocessing-workflow-with-python/
-
-
-
+def main():
+    district = input("Enter the name of your town district: ")
+    print(district)
 
 
 #Zack's workflow to be turned into script:
 
-# #project shapefile
-# arcpy.managment.Project(‘.shp file’, ‘Town_32145’, ‘32145’)
-# #project TIGER
-# arcpy.managment.Project(‘TIGER file’, ‘TIGER_32145’, ‘32145’)
-# #arcpy command to repair geometry for both the TIGER and 32145
-# arcpy.management.RepairGeometry(‘Town.gdb/Town_32145’)
-# arcpy.management.RepairGeometry(‘Town.gdb/TIGER_32145’)
-# #arcpy command to enable snaping and set snap tolerance to like 100
-# #maybe just do this manually
+def geoProcesses(town):
 
-# #arcpy command to snap vertex for like 50 meters
-# #arcpy command to snap edge for like 50 meters
-# arcpy.edit.Snap(‘Town.gdb/Town_32145’,
-#         [[‘Town.gdb/TIGER_32145’, ‘VERTEX’, ’50 Meters’],
-#          [‘Town.gdb/TIGER_32145’, ‘EDGE’, ’50 Meters’]])
+    #project shapefile
+    arcpy.managment.Project('.shp file', town+'_32145', '32145')
 
-# #Topology must be last step before manual edits. Datasets LOCK FEATURE CLASSES IN, because administration hates the working man! Idk its stupid. //
-# #create dataset
-# arcpy.managment.CreateFeatureDataset(‘Town.gdb’, ‘Town’)
 
-# #move files into dataset
-# arcpy.management.TransferFiles(‘Town.gdb/Town_32145’, ‘Town.gdb/Town’,)
-# arcpy.management.TransferFiles(‘Town.gdb/TIGER_32145’, ‘Town.gdb/Town’,)
+    #project TIGER
+    arcpy.managment.Project('TIGER file', 'TIGER_32145', '32145')
+    #arcpy command to repair geometry for both the TIGER and 32145
+    arcpy.management.RepairGeometry(town+'.gdb/'+town+'_32145')
+    arcpy.management.RepairGeometry(town+'.gdb/TIGER_32145')
+    #arcpy command to enable snaping and set snap tolerance to like 100
+    #maybe just do this manually
 
-# #create overlap/gap topology
-# arcpy.managment.CreateTopology(‘Town.gdb/Town’, ‘Town_Zoning_Topology’, ‘1’)
+    #arcpy command to snap vertex for like 50 meters
+    #arcpy command to snap edge for like 50 meters
+    arcpy.edit.Snap(town+'.gdb/'+town+'_32145',
+            [[town+'.gdb/TIGER_32145', 'VERTEX', '50 Meters'],
+            [town+'.gdb/TIGER_32145', 'EDGE', '50 Meters']])
 
-# #add 32145 to overlap/gap topology
-# arcpy.managment.AddFeatureClassToTopology(‘Town.gdb/Town/Town_Zoning_Topology’, ‘Town.gdb/Town/Town_32145’, ‘1’, ‘1’)
+    #Topology must be last step before manual edits. Datasets LOCK FEATURE CLASSES IN.
+    #create dataset
+    arcpy.managment.CreateFeatureDataset(town+'.gdb', town)
 
-# #add rules to overlap/gap
-# arcpy.managment.AddRuleToTopology(‘Town.gdb/Town/Town_Zoning_Topology’, ‘Must Not Have Gaps (Area)’, ‘Town.gdb/Town/Town_32145’)
-# arcpy.managment.AddRuleToTopology(‘Town.gdb/Town/Town_Zoning_Topology’, ‘Must Not Overlap (Area)’, ‘Town.gdb/Town/Town_32145’)
+    #move files into dataset
+    arcpy.management.TransferFiles(town+'.gdb/'+town+'_32145', town+'.gdb/'+town,)
+    arcpy.management.TransferFiles(town+'.gdb/TIGER_32145', town+'.gdb/'+town,)
+
+    #create overlap/gap topology
+    arcpy.managment.CreateTopology(town+'.gdb/'+town, town+'_Zoning_Topology', '1')
+
+    #add 32145 to overlap/gap topology
+    arcpy.managment.AddFeatureClassToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', town+'.gdb/'+town+'/'+town+'_32145', '1', '1')
+
+    #add rules to overlap/gap
+    arcpy.managment.AddRuleToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', 'Must Not Have Gaps (Area)', town+'.gdb/'+town+'/'+town+'_32145')
+    arcpy.managment.AddRuleToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', 'Must Not Overlap (Area)', town+'.gdb/'+town+'/'+town+'_32145')
+    
+main()
