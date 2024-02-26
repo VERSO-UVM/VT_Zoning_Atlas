9ed3700bad892fe732af092f6c3618aeef8e8531
ZWinigrad <153955538+ZWinigrad@users.noreply.github.com>
2024-02-26 13:44:11 -0500

    fixed typos

--- ZoningAtlas/ArcPy/script.py
+++ ZoningAtlas/ArcPy/script.py
@@ -1,6 +1,4 @@
-#importing arcpy tools
-import arcpy #this is just to avoide annoying warnings while editing in ide
-
+import arcpy
 def main():
     district = input("Enter the name of your town district: ")
     #For confirmation of correct district input
@@ -13,11 +11,11 @@ def main():
 def geoProcesses(town):
 
     #project shapefile
-    arcpy.managment.Project('.shp file', town+'_32145', '32145')
+    arcpy.management.Project(town, town+'_32145', '32145')
 
 
     #project TIGER
-    arcpy.managment.Project('TIGER file', 'TIGER_32145', '32145')
+    arcpy.management.Project('TIGER', 'TIGER_32145', '32145')
     #arcpy command to repair geometry for both the TIGER and 32145
     arcpy.management.RepairGeometry(town+'.gdb/'+town+'_32145')
     arcpy.management.RepairGeometry(town+'.gdb/TIGER_32145')
@@ -32,20 +30,20 @@ def geoProcesses(town):
 
     #Topology must be last step before manual edits. Datasets LOCK FEATURE CLASSES IN.
     #create dataset
-    arcpy.managment.CreateFeatureDataset(town+'.gdb', town)
+    arcpy.management.CreateFeatureDataset(town+'.gdb', town)
 
     #move files into dataset
     arcpy.management.TransferFiles(town+'.gdb/'+town+'_32145', town+'.gdb/'+town,)
     arcpy.management.TransferFiles(town+'.gdb/TIGER_32145', town+'.gdb/'+town,)
 
     #create overlap/gap topology
-    arcpy.managment.CreateTopology(town+'.gdb/'+town, town+'_Zoning_Topology', '1')
+    arcpy.management.CreateTopology(town+'.gdb/'+town, town+'_Zoning_Topology', '1')
 
     #add 32145 to overlap/gap topology
-    arcpy.managment.AddFeatureClassToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', town+'.gdb/'+town+'/'+town+'_32145', '1', '1')
+    arcpy.management.AddFeatureClassToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', town+'.gdb/'+town+'/'+town+'_32145', '1', '1')
 
     #add rules to overlap/gap
-    arcpy.managment.AddRuleToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', 'Must Not Have Gaps (Area)', town+'.gdb/'+town+'/'+town+'_32145')
-    arcpy.managment.AddRuleToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', 'Must Not Overlap (Area)', town+'.gdb/'+town+'/'+town+'_32145')
+    arcpy.management.AddRuleToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', 'Must Not Have Gaps (Area)', town+'.gdb/'+town+'/'+town+'_32145')
+    arcpy.management.AddRuleToTopology(town+'.gdb/'+town+'/'+town+'_Zoning_Topology', 'Must Not Overlap (Area)', town+'.gdb/'+town+'/'+town+'_32145')
     
 main()
